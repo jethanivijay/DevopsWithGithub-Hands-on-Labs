d5e6c3fc6dfb9beb7f63ebf565a4cea49cdd00e2
jethanivijay <66484052+jethanivijay@users.noreply.github.com>
2020-12-07 14:14:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('Welcome to Github Hands on Labs !');
 });
 app.listen(process.env.PORT, function () {
   console.log('Example app listening on process.env.PORT');
