0d011dd3334c6328b367a71391369e8ff07286ba
Vijay Jethani <abc@org.com>
2021-03-23 14:58:01 +0530

    Code Update

---
+++ Application/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<title>PHP Application</title>
+	<link href="css/site.css" rel="stylesheet">
+</head>
+
+<body>
+<div class="main-container">
+            <div class="content-body">
+                <div class="success-text">Well done !</div>
+                <div class="description line-1">
+				<?php
+				echo "Welcome to Github Hands on Lab Session ! ";
+				?>
+				</div>
+                <div class="description line-2">
+				<?php
+				$appType = "PHP";
+				echo "Your $appType app is up and running on Azure now";
+				?>
+				</div>
+
+        </div>
+    </div>
+</body>
+</html>
